b9186cd8570e775c662062a3526a90259a89ea06
benswagg04@gmail.com <benswagg04@gmail.com>
2022-03-16 09:38:17 -0700

    my commit task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*main - entry point
-*my hello, print task
-*Return: return 1 after program excecution
+*main-entry point
+*
+*Return: 1 after printing the function
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,"/n);
+printf(with proper grammar, but the outcome is a piece of art,/n");
 return (0);
 }
